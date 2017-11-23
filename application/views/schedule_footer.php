0998dbfef97e13ff0909d778e24112911a6f0794
farancak <farancak@gmail.com>
2017-11-23 15:01:29 +0700

    first push

---
+++ application/views/schedule_footer.php
@@ -0,0 +1,8 @@
+<script src="<?php echo base_url();?>assets/js/modernizr.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+<script>
+	if( !window.jQuery ) document.write('<script src="<?php echo base_url();?>assets/js/jquery-3.0.0.min.js"><\/script>');
+</script>
+<script src="<?php echo base_url();?>assets/js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
---
+++ application/views/schedule_header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css"> <!-- Resource style -->
+  	
+	<title>APTIKA</title>
+</head>
